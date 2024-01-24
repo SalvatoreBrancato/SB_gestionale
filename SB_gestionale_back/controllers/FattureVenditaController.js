f6b2f5388d336d3f2e41d13167a1b2b9c9aa99cf
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2024-01-24 14:37:21 +0100

    router/controller fatture vendita

--- SB_gestionale_back/controllers/FattureVenditaController.js
+++ SB_gestionale_back/controllers/FattureVenditaController.js
@@ -1,25 +1,108 @@
 const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
+//###INDEX###
 async function index(req, res){
-    
+    const data = await prisma.fattureVendita.findMany({
+        include:{
+            clienti:{
+                select:{
+                    ragioneSociale: true
+                }
+            }
+        }
+    })
+
+    return res.json(data)
 }
 
+//###SHOW###
 async function show(req, res){
+    const id= req.params.id
 
+    const data = await prisma.fattureVendita.findUnique({
+        where:{
+            id: parseInt(id)
+        },
+        include:{
+            clienti:{
+                select:{
+                    ragioneSociale: true
+                }
+            }
+        }
+    })
+    
+    if (!data) {
+        throw new Error("Fattura non trovata");
+    }
+
+    return res.json(data)
 }
 
+//###CREATE###
 async function create(req, res){
-    
+    const datiInIngresso = req.body
+
+    const nuovaFatturaVendita = await prisma.fattureVendita.create({
+        data: {
+            numero: datiInIngresso.numero,
+            data: datiInIngresso.data,
+            pezzi: datiInIngresso.pezzi,
+            iva: datiInIngresso.iva,
+            listino: datiInIngresso.listino,
+            sconto: datiInIngresso.sconto,
+            totale: datiInIngresso.totale,
+            note: datiInIngresso.note,
+            clientiId: datiInIngresso.clientiId            
+        }
+    })
+
+    return res.json(nuovaFatturaVendita)
 }
 
+//###UPDATE###
 async function update(req, res){
-    
+    const id = req.params.id
+
+    const datiInIngresso = req.body
+
+    //controllo se la fattura esiste
+    const fatturaVendita = await prisma.fattureVendita.findUnique({
+        where:{
+            id: parseInt(id)
+        }
+    })
+
+    if(!fatturaVendita){
+        throw new Error('Fattura vendita non trovata')
+    }
+
+    const aggiornaFatturaVendita = await prisma.fattureVendita.update({
+        where:{
+            id: parseInt(id)
+        },
+        data:{
+            ...datiInIngresso
+        }
+    })
+
+    return res.json(aggiornaFatturaVendita)
 }
 
+//###DESTROY###
 async function destroy(req, res){
-    
+    const id = req.params.id
+    await prisma.fattureVendita.delete({
+        where:{
+            id: parseInt(id)
+        }
+    })
+    return res.json({message:'Fattura eliminata'})
 }
+
+
+
 module.exports = {
     index,
     show,
--- SB_gestionale_back/routers/FattureVenditaRouter.js
+++ SB_gestionale_back/routers/FattureVenditaRouter.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const fattureVenditaController = require('../controllers/fattureVenditaController');
 
+router.get('/', fattureVenditaController.index);
+router.post('/inserisci', fattureVenditaController.create);
+router.put('/modifica/:id', fattureVenditaController.update);
+router.get('/:id', fattureVenditaController.show);
+router.delete('/:id', fattureVenditaController.destroy)
 
 module.exports = router
