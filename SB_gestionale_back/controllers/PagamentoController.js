cf928fedcfa760295e33d77c3230f9f160f45914
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2024-01-25 13:01:21 +0100

    router/controller pagamento

--- SB_gestionale_back/controllers/PagamentoController.js
+++ SB_gestionale_back/controllers/PagamentoController.js
@@ -1,25 +1,95 @@
 const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
+//###INDEX###
 async function index(req, res){
-    
+    const data = await prisma.pagamento.findMany()
+    return res.json(data)
 }
 
+//###SHOW###
 async function show(req, res){
+    const id = req.params.id
 
+    const data = await prisma.pagamento.findUnique({
+        where:{
+            id: parseInt(id)
+        },
+        include:{
+            fattureAcquisti:{
+                select:{
+                    numero: true,
+                    data: true,
+                    totale: true,
+                    note: true,
+                    fornitoriId: true
+                }
+            }
+        }
+    })
+
+    if (!data) {
+        throw new Error("Pagamento non trovato");
+    }
+
+    return res.json(data)
 }
 
+//###CREATE###
 async function create(req, res){
-    
+    const datiInIngresso = req.body
+
+    const nuovoPagamento = await prisma.pagamento.create({
+        data:{
+            scadenza: datiInIngresso.scadenza,
+            note: datiInIngresso.note
+        }
+    })
+
+    return res.json(nuovoPagamento)
 }
 
+//###UPDATE###
 async function update(req, res){
-    
+    const id = req.params.id
+    const datiInIngresso = req.body
+
+    const pagamento = await prisma.pagamento.findUnique({
+        where:{
+            id:parseInt(id)
+        }
+    })
+
+    if(!pagamento){
+        throw new Error('pagamento non trovato')
+    }
+
+    const aggiornaPagamento = await prisma.pagamento.update({
+        where:{
+            id: parseInt(id)
+        },
+        data:{
+            ...datiInIngresso
+        }
+    })
+
+    return res.json(aggiornaPagamento)
 }
 
+
+//###DESTROY###
 async function destroy(req, res){
-    
+    const id = req.params.id
+
+    await prisma.pagamento.delete({
+        where:{
+            id: parseInt(id)
+        }
+    })
+    return res.json({message: 'Pagamento eliminato'})
 }
+
+
 module.exports = {
     index,
     show,
--- SB_gestionale_back/routers/PagamentoRouter.js
+++ SB_gestionale_back/routers/PagamentoRouter.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const pagamentoController = require('../controllers/pagamentoController');
 
+router.get('/', pagamentoController.index);
+router.post('/inserisci', pagamentoController.create);
+router.put('/modifica/:id', pagamentoController.update);
+router.get('/:id', pagamentoController.show);
+router.delete('/:id', pagamentoController.destroy);
 
 module.exports = router
