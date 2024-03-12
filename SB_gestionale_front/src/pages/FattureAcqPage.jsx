89e7d342db57fb0399fd1e992f4e68d79ca5391d
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2024-03-12 13:14:47 +0100

    dimensione icone

--- SB_gestionale_front/src/pages/FattureAcqPage.jsx
+++ SB_gestionale_front/src/pages/FattureAcqPage.jsx
@@ -80,7 +80,7 @@ export default function FattureAcqPage() {
                 </div>
                 <div className='flex'>
                     <button className='flex flex-col justify-center items-center' onClick={() => apriChiudiForm()}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="#03A9F4" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#FFFFFF" className="w-9 h-9 transform hover:scale-125">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#03A9F4" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#FFFFFF" className="w-8 h-8 transform hover:scale-125">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                         </svg>
                     </button>
